5bfbe79d72516f150572800f3ad18e6e59915492
Uros Hekic <uros.hekic@gmail.com>
2018-03-04 10:31:22 +0100

    Add Sedgewick's dual-pivot partitioning

---
+++ src/algs/QuicksortSedgewickDualPivot.h
@@ -0,0 +1,47 @@
+typedef int bool;
+#define true 1
+#define false 0
+
+
+void FUNNAME(int a[], int left, int right) {
+	if (right <= left) return;
+	CALLED;
+	int i = left; int i1 = left;
+	int j = right; int j1 = right;
+
+	if (GT(a[left], a[right])) SWAP(a, left, right);
+	int p = a[left]; int q = a[right];
+    while (true) {
+        bool breakOuterLoop = false;
+
+        i++;
+        while (LE(a[i], q)) {
+            if (i >= j) { breakOuterLoop = true; break; }
+            if (LT(a[i], p)) {
+                a[i1] = a[i]; i1++; a[i] = a[i1];
+            }
+            i++;
+        }
+        if (breakOuterLoop) break;
+
+        j--;
+        while (GE(a[j], p)) {
+            if (GT(a[j], q)) {
+                a[j1] = a[j]; j1--; a[j] = a[j1];
+            }
+            if (i >= j) { breakOuterLoop = true; break; }
+            j--;
+        }
+        if (breakOuterLoop) break;
+
+        a[i1] = a[j]; a[j1] = a[i];
+        i1++; j1--;
+        a[i] = a[i1]; a[j] = a[j1];
+    }
+
+    a[i1] = p; a[j1] = q;
+
+	FUNNAME(a, left, i1 - 1);
+	FUNNAME(a, i1 + 1, j1 - 1);
+	FUNNAME(a, j1 + 1, right);
+}
